6c339429611d35d76605be899214cc63c6a45311
Zahra-H.N <zy14333@gmail.com>
2023-09-28 23:56:12 +0300

    added file variables

---
+++ src/variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age = 19;
+  String name = 'Zahra';
+  bool isStudent = true;
+  List<String> hobbies = ['coding', 'sport', 'drawing', 'writing'];
+
+  //print variables
+  print('Age: $age');
+  print('Name: $name');
+  print('IsStudent: $isStudent');
+  print('Hobbies: $hobbies');
+}
