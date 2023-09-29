da10dd958949cc708306afb9032293152bb34fe7
Zahra-H.N <zy14333@gmail.com>
2023-09-29 12:25:29 +0300

    added task file

--- src/variables.dart
+++ src/variables.dart
@@ -1,17 +1,28 @@
-void main() {
+/* void main() {
   int age = 19;
   String name = 'Zahra';
   bool isStudent = true;
-  List<String> hobbies = ['coding', 'sport', 'drawing', 'writing'];
+  List<String> hobbies = ['coding', 'sport', 'drawing', 'writing', 'reading', 'cooking'];
 
   //print variables
   print('Age: $age');
   print('Name: $name');
   print('IsStudent: $isStudent');
   print('Hobbies: $hobbies');
+}*/
+
+import 'task.dart';
+
+void main() {
+  Task task1 = Task(1, 'Buy groceries', 'Buy milk, eggs, and bread', false);
+  Task task2 = Task(2, 'Read a book', 'Read "The Catcher in the Rye"', true);
+
+  //print task properties
+  print('Task 1: ${task1.title}, Completed: ${task1.isCompleted}');
+  print('task 2: ${task2.title}, Completed: ${task2.isCompleted}');
 }
 
-void main2() {
+void Main() {
   try {
     int result = 10 ~/ 0; //triggering error
     print("Result: $result");
